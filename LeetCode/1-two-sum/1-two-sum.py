d96bc39bc132f4e94242d31b47b4baefae5c5b2f
Miyeon-Hwang <102529294+Miyeon-Hwang@users.noreply.github.com>
2022-04-22 00:26:16 +0900

    Time: 90 ms (60.48%), Space: 15.2 MB (25.32%) - LeetHub

--- LeetCode/1-two-sum/1-two-sum.py
+++ LeetCode/1-two-sum/1-two-sum.py
@@ -1,7 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numHash = {}
         for i, n in enumerate(nums):
-            for j in range(i + 1, len(nums)):
-                if n + nums[j] == target:
-                    return [i, j]
+            sub_n = target - n
+            if sub_n in numHash:
+                return [i, numHash[sub_n]]
+            numHash[n] = i
                     
