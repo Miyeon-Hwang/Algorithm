60c5bd20db706633e3d9e6c0f88e7ce117697d9f
Miyeon-Hwang <myhwang.dev@gmail.com>
2022-05-15 15:03:47 +0900

    Time: 48 ms (75.30%), Space: 14 MB (60.24%) - LeetHub

--- LeetCode/395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
+++ LeetCode/395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
@@ -8,6 +8,7 @@ class Solution:
         # cnt_char = Counter(s) # Counter를 여기서 구하는게 재귀돌때마다 다 구하니까 오래걸리네..
         for i, c in enumerate(s):
             if s.count(c) < k:
-                return max(self.longestSubstring(s[:i], k), self.longestSubstring(s[i+1:], k))
+                #return max(self.longestSubstring(s[:i], k), self.longestSubstring(s[i+1:], k)) # 리스트 복사라 오래걸리나?
+                return max(self.longestSubstring(sub, k) for sub in s.split(c))
         return len(s)
             
