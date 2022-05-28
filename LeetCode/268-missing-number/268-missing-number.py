762ed7be53af1cdf21d42b2ea69bac2ce24060b2
Miyeon-Hwang <myhwang.dev@gmail.com>
2022-05-28 17:16:18 +0900

    Time: 206 ms (42.10%), Space: 15.3 MB (34.52%) - LeetHub

--- LeetCode/268-missing-number/268-missing-number.py
+++ LeetCode/268-missing-number/268-missing-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maxn = max(nums)
-        if len(nums) == maxn:
-            return sum([i for i in range(maxn + 1)]) - sum(nums)
-        return len(nums)
+        # maxn = max(nums)
+        # if len(nums) == maxn:
+        #     return sum([i for i in range(maxn + 1)]) - sum(nums)
+        # return len(nums)
+        n = len(nums)
+        return n*(n+1)//2 - sum(nums)
