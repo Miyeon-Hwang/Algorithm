af4db85c4d78243f72ff970eb891bc19969f5ff9
Miyeon-Hwang <myhwang.dev@gmail.com>
2022-05-31 00:10:23 +0900

    Time: 32 ms (94.21%), Space: 14 MB (49.80%) - LeetHub

--- LeetCode/14-longest-common-prefix/14-longest-common-prefix.py
+++ LeetCode/14-longest-common-prefix/14-longest-common-prefix.py
@@ -2,19 +2,22 @@ from collections import defaultdict
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        d = defaultdict(int)
-        for s in strs:
-            for i in range(len(s)):
-                d[s[:i+1]] += 1
-        
-        if "" in d:
+        min_str = min(strs, key=len)
+        if min_str == "":
             return ""
-        
-        items = list(item for item in d.items() if item[1] == len(strs))
-        if not items:
-            return ""
-        items.sort(reverse=True, key=lambda x: len(x[0]))
-        return items[0][0]
+        for str in strs:
+            for i, c in enumerate(str):
+                if i > len(min_str) - 1:
+                    break
+                if c != min_str[i]:
+                    min_str = min_str[:i]
+                    if min_str == "":
+                        return ""
+                    break
+        return min_str
+                    
+                    
+                
                 
         
         
