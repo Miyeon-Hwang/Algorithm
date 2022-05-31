2692751bb34bc430386ad4492200922bfd1b6e47
Miyeon-Hwang <myhwang.dev@gmail.com>
2022-05-31 23:22:55 +0900

    Time: 109 ms (47.22%), Space: 13.9 MB (86.57%) - LeetHub

--- LeetCode/1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
+++ LeetCode/1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
@@ -1,27 +1,16 @@
-from collections import defaultdict
-
 class Solution:
     def countSubstrings(self, s: str, t: str) -> int:
         answer = 0
-        d = defaultdict(int)
-        for i, c in enumerate(s):
-            for j in range(i+1, len(s)+1):
-                substr = s[i:j]
-                if substr in d:
-                    answer += d[substr]
-                    continue
-                for n in range(len(t) - len(substr) + 1):
-                    diff_cnt = 0
-                    for ti, tc in enumerate(t[n:n+len(substr)]):
-                        if tc != substr[ti]:
-                            diff_cnt += 1
-                            if diff_cnt > 1:
-                                break
-                    if diff_cnt == 1:
-                        d[substr] += 1
-                
-        print(d)
-        return answer + sum(d.values())
+        for i in range(len(s)):
+            for j in range(len(t)):
+                length = diff = 0
+                while i + length < len(s) and j + length < len(t) and diff <= 1:
+                    if s[i+length] != t[j+length]:
+                        diff += 1
+                    if diff == 1:
+                        answer += 1
+                    length += 1
+        return answer
                         
                     
                 
